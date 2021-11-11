4e60910bc9d0a3c5682aae17584909f2a3e62698
norton16 <bnorton184@gmail.com>
2021-11-11 17:27:11 -0600

    albums vuex store

--- src/store/albums/albums.js
+++ src/store/albums/albums.js
@@ -1,13 +1,44 @@
+import { API, graphqlOperation } from 'aws-amplify'
+import { createAlbum as createAlbumMutation } from '@/graphql/mutations'
+import { getAlbum as getAlbumQuery } from '@/graphql/queries'
+import { listAlbums as listAlbumsQuery } from '@/graphql/queries'
+
+// Vuex store for albums
 export const albumInfo = {
     namespaced: true,
-    state: { error: null, albums: null },
+
+    // State
+    state: { albums: null },
+    
+    // Mutations
     mutations: {
-
+        setAlbums(state, payload) {
+            state.albums = payload;
+        }
     },
+
+    // Actions
     actions: {
-
+        async createAlbum(_, newAlbum) {
+            try {
+                await API.graphql(graphqlOperation(createAlbumMutation, {input: newAlbum}));
+            } catch (error) {
+                console.log("createalbum", error);
+            }
+        },
+        async getAlbum(_, albumId) {
+            return await API.graphql(
+                graphqlOperation(getAlbumQuery, {id: albumId})
+            )
+        },
+        async getAlbumsData({ commit }) {
+            const albumsData = await API.graphql(graphqlOperation(listAlbumsQuery));
+            commit("setAlbums", albumsData.data.listAlbums.items);
+        }
     },
+
+    // Getters
     getters: {
-        
+        albums: (state) => state.albums
     }
 }
